7bc8b8070129f0332a9d39716bde93654026f4e6
Rhea <rheam97@gmail.com>
2022-04-12 10:16:25 -0400

    added react components for thoughts, concurrent dev environment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const {ApolloServer} = require('apollo-server-express')
 const {typeDefs, resolvers} = require('./schemas')
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const path = require('path')
 
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -24,6 +25,14 @@ startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+app.get('*', (req, res)=> {
+  res.sendFile(path.join(__dirname, '../clien/build/index.htmlt'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
