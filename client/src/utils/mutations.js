c2d15a0a67a6c82b468e75afc68bfe40deb0f372
Rhea <rheam97@gmail.com>
2022-04-21 18:35:08 -0400

    not sure why reactions are not submitting text

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -50,3 +50,17 @@ export const ADD_THOUGHT = gql`
     }
   }
 `;
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
